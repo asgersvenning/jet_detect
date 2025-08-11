f68ce42bb43b1cf7409039150c23b15e711fa041
Asger Svenning <82270047+asgersvenning@users.noreply.github.com>
2025-08-11 23:28:45 +0200

    Increase number of queued batches in RemotePathIterator to enhance network buffering

--- predict.py
+++ predict.py
@@ -253,7 +253,7 @@ def main(
     ):
     with IOHandler() as io:
         io.cd(input)
-        rpi = RemotePathIterator(io, clear_local=True, store=True, batch_parallel=14, n_local_files=512, max_queued_batches=6)
+        rpi = RemotePathIterator(io, clear_local=True, store=True, batch_parallel=14, n_local_files=4096, max_queued_batches=32)
         dst_check = {
             p : name for p in rpi.remote_paths 
             if is_image(p) 
